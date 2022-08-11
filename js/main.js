58bbbba6baaddbb5c5f8dfd4fa3481c576aa907c
Bamidele <bamsjovicy@gmail.com>
2022-08-11 23:25:18 +0100

    making progress with the carousel

--- js/main.js
+++ js/main.js
@@ -7,51 +7,79 @@ toggleBtn.addEventListener("click", function(){
 });
 
 // Carousel Part
-var span = document.getElementsByTagName('span');
-var modal = document.getElementsByTagName("div");
-var l = 0;
-span[1].onclick = ()=> {
-    l++;
-    for(var i of modal) {
-        if (l==0) {
-            i.style.left = "0px";
-        }
-        if (l==1) {
-            i.style.left = "-740px";
-        }
-        if (l==2) {
-            i.style.left = "-1480px";
-        }
-        if (l==3) {
-            i.style.left = "-2220px";
-        }
-        if (l==4) {
-            i.style.left = "-2960px";
-        }
-        if (l>4) {
-            l = 4;
-        }
-    }
+// var span = document.getElementsByTagName('span');
+// var modal = document.getElementsByTagName("div");
+// var l = 0;
+// span[1].onclick = ()=> {
+//     l++;
+//     for(var i of modal) {
+//         if (l==0) {
+//             i.style.left = "0px";
+//         }
+//         if (l==1) {
+//             i.style.left = "-740px";
+//         }
+//         if (l==2) {
+//             i.style.left = "-1480px";
+//         }
+//         if (l==3) {
+//             i.style.left = "-2220px";
+//         }
+//         if (l==4) {
+//             i.style.left = "-2960px";
+//         }
+//         if (l>4) {
+//             l = 4;
+//         }
+//     }
+// }
+
+// span[0].onclick = ()=> {
+//     l--;
+//     for(var i of modal) {
+//         if (l==0) {
+//             i.style.left = "0px";
+//         }
+//         if (l==1) {
+//             i.style.left = "-740px";
+//         }
+//         if (l==2) {
+//             i.style.left = "-1480px";
+//         }
+//         if (l==3) {
+//             i.style.left = "-2220px";
+//         }
+//         if (l<0) {
+//             l = 0;
+//         }
+//     }
+// }
+// End of Carousel
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-span[0].onclick = ()=> {
-    l--;
-    for(var i of modal) {
-        if (l==0) {
-            i.style.left = "0px";
-        }
-        if (l==1) {
-            i.style.left = "-740px";
-        }
-        if (l==2) {
-            i.style.left = "-1480px";
-        }
-        if (l==3) {
-            i.style.left = "-2220px";
-        }
-        if (l<0) {
-            l = 0;
-        }
-    }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-// End of Carousel
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
